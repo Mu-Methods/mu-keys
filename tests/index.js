f630e8f866c578379c4dcfd4d1d52b8ec7e9ef96
frankiebee <frankie.diamond@gmail.com>
2023-01-17 13:20:18 -0800

    meta: test to-dos

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,6 @@
 const test = require('tape')
 const plugin = require('../build')
-
+const bip39 = require('bip39')
 test('should have exist and qualify spec', async (t) => {
   t.plan(6)
   console.log('plugin', plugin)
@@ -11,11 +11,13 @@ test('should have exist and qualify spec', async (t) => {
   t.ok(plugin.init, 'plugin has an init function')
   t.deepEqual(Object.keys(plugin.manifest), Object.keys(plugin.init()),'plugins init object functions match whats listed in manifest')
 
+  // TO-DO's tests:
 
+  // createMnemonic: should generate a valid mnemonic
 
+  // useMnemonic && createNewKeys: should use the correct mnemonic to create the correct keys
 
-
-
+  // createNewKeys: should create keys with next index
 
 
 
